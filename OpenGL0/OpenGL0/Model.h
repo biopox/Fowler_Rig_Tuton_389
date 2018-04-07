8da9563cfcbe73b284f87d4ec8433df177dff52e
cptuton <mousewarrior209@gmail.com>
2018-04-07 14:55:32 -0600

    Added class template for OBJ
    Added class template for OBJ containing much useful information, added functionality for printing the loaded model

--- OpenGL0/OpenGL0/Model.h
+++ OpenGL0/OpenGL0/Model.h
@@ -22,8 +22,25 @@
 #include <assimp\scene.h>
 #include <assimp\postprocess.h>
 
-
-
 using namespace std;
-int loadOBJ(string path, float *triangles, float scale);
+
+class OBJ {
+public:
+	vector<float> vertices;
+	vector<float> textures;
+	vector<float> normals;
+	vector<int> faces;			// vertices of each face
+	vector<int> facesNormal;	// normals of each face
+	vector<int> facesTexture;	// texture coordinates of each face
+	bool verticesIncludeW;		// If .obj has v x y z [w]
+	bool texturesIncludeW;		// If .obj has vt u v [w]
+	int facesSize;				// Size of face e.g. triangles are 3
+	int faceMode;				// v:		0
+								// v, t:	1
+								// v, t, n: 2
+								// v, n:	3
+};
+
+OBJ loadOBJ(string path, float scale);
+void printOBJ(OBJ obj);
 GLint TextureFromFile(const char *path, string directory);
