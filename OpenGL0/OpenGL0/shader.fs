1b03b89608256d6f694d96a2cacaf4336bcefbed
biopox <benjamin.fowler@student.nmt.edu>
2018-04-08 00:25:55 -0600

    Update shader.fs

--- OpenGL0/OpenGL0/shader.fs
+++ OpenGL0/OpenGL0/shader.fs
@@ -10,5 +10,5 @@ uniform sampler2D texture2;
 
 void main()
 {
-	FragColor = texture(texture1, TexCoord) * vec4(ourColor, 1.0f);
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
