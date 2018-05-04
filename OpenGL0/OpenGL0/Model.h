86a56cba203cfe843c792f2d5c7c50e222d36d1d
cptuton <mousewarrior209@gmail.com>
2018-05-04 17:52:29 -0600

    Update model.h for compatibility reasons

--- OpenGL0/OpenGL0/Model.h
+++ OpenGL0/OpenGL0/Model.h
@@ -29,23 +29,26 @@ using namespace std;
 
 class OBJ {
 public:
-	vector<float> vertices;
-	vector<float> textures;
-	vector<float> normals;
-	vector<int> faces;			// vertices of each face
-	vector<int> facesNormal;	// normals of each face
-	vector<int> facesTexture;	// texture coordinates of each face
-	bool verticesIncludeW;		// If .obj has v x y z [w]
-	bool texturesIncludeW;		// If .obj has vt u v [w]
-	int facesSize;				// Size of face e.g. triangles are 3
-	int faceMode;				// v:		0
-								// v, t:	1
-								// v, t, n: 2
-								// v, n:	3
-	string name;				// Name of object
+    vector<float> vertices;
+    vector<float> textures;
+    vector<float> normals;
+    vector<int> faces;            // vertices of each face
+    vector<int> facesNormal;    // normals of each face
+    vector<int> facesTexture;    // texture coordinates of each face
+    bool verticesIncludeW;        // If .obj has v x y z [w]
+    bool texturesIncludeW;        // If .obj has vt u v [w]
+    int facesSize;                // Size of face e.g. triangles are 3
+    int faceMode;                // v:        0
+                                // v, t:    1
+                                // v, t, n: 2
+                                // v, n:    3
+    string name;                // Name of object
 };
 
 vector<OBJ> loadOBJ(string path, float scale);
 void printOBJ(vector<OBJ> objects);
+float *verticesToFloat(OBJ obj);
+float *getVerticesArray(OBJ obj);
+int *getFacesArray(OBJ obj);
 
 GLint TextureFromFile(const char *path, string directory);
